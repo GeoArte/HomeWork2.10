a3143aaa98d2a99985602091a0cf0b68572986db
gosha <gosha.artemov@gmail.com>
2023-03-10 00:54:59 +0400

    HomeWork done2

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.function.Supplier;
 public class Main {
     public static void main(String[] args)
     {
-        Predicate<Integer> Checker = new Predicate<Integer>() {
+        Predicate<Integer> checker = new Predicate<Integer>() {
             @Override
             public boolean test(Integer integer) {
                 if (integer >= 0)
@@ -21,8 +21,8 @@ public class Main {
                 }
             }
         };
-        Predicate<Integer> CheckerL = (a) -> a>= 0;
-        Consumer<String> SeyHelloToMyLittleFriend = new Consumer()
+        Predicate<Integer> checkerL = (a) -> a>= 0;
+        Consumer<String> seyHelloToMyLittleFriend = new Consumer()
         {
             @Override
             public void accept(Object o)
@@ -30,11 +30,11 @@ public class Main {
                 System.out.println("Hello, " + o + "!");
             }
         };
-        Consumer<String> SeyHelloToMyLittleFriendL = (name) ->
+        Consumer<String> seyHelloToMyLittleFriendL = (name) ->
         {
             System.out.println("Hello, " + name + "!");
         };
-        Function<Double, Long> Transformation = new Function<Double, Long>()
+        Function<Double, Long> transformation = new Function<Double, Long>()
         {
             @Override
             public Long apply(Double o)
@@ -43,12 +43,12 @@ public class Main {
                 return a;
             }
         };
-        Function<Double, Long> TransformationL = (o) ->
+        Function<Double, Long> transformationL = (o) ->
         {
             Long a = o.longValue();
             return a;
         };
-        Supplier<Integer> CreateRandomNumber = new Supplier() {
+        Supplier<Integer> createRandomNumber = new Supplier() {
             @Override
             public Object get() {
                 Random random = new Random();
@@ -56,7 +56,7 @@ public class Main {
                 return r;
             }
         };
-        Supplier<Integer> CreateRandomNumberL = () ->
+        Supplier<Integer> createRandomNumberL = () ->
         {
             Random random = new Random();
             Integer r = random.nextInt(101 - 1) + 1;
@@ -65,13 +65,22 @@ public class Main {
         int b = 5;
         int c = -6;
         double d = 2.86948;
-        System.out.println(Checker.test(b));
-        System.out.println(CheckerL.test(c));
-        SeyHelloToMyLittleFriend.accept("Georgy");
-        SeyHelloToMyLittleFriendL.accept("Lena");
-        System.out.println(Transformation.apply(d));
-        System.out.println(TransformationL.apply(d));
-        System.out.println(CreateRandomNumber.get());
-        System.out.println(CreateRandomNumberL.get());
+        System.out.println(checker.test(b));
+        System.out.println(checkerL.test(c));
+        seyHelloToMyLittleFriend.accept("Georgy");
+        seyHelloToMyLittleFriendL.accept("Lena");
+        System.out.println(transformation.apply(d));
+        System.out.println(transformationL.apply(d));
+        System.out.println(createRandomNumber.get());
+        System.out.println(createRandomNumberL.get());
+
+        public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse)
+        {
+            return;
+        }
+
     }
 }
